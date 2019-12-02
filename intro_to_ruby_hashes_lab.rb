5b1a24b5540d9446340c226be085f6b90a0f488a
Daniel Liu <danliu277@gmail.com>
2019-12-02 20:11:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,8 +13,7 @@ def monopoly_with_second_tier
   # complexity.
 	{
     :railroads=>{
-      :pieces=>4,
-      :rent_in_dollars=>{}
+      :pieces=>4
     }
 	}
 end
@@ -26,7 +25,8 @@ def monopoly_with_third_tier
   # complexity.
   {
     :railroads=>{
-      :pieces=>4
+      :pieces=>4,
+      :rent_in_dollars=>{}
     }
 	}
 end
